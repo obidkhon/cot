dd761fb825b591741de2c7f97f5ac2cf08f3d6e2
obidkon <obid.sharipov.80@mail.ru>
2020-11-17 21:16:13 +0500

    ok

---
+++ src/containts.java
@@ -0,0 +1,32 @@
+public class containts {
+    public static void main(String[] args) {
+        // Continents and their largest cities.
+        int continent=6;
+        switch (continent){
+            case 1:
+                System.out.println("North America: Mexico City, Mexico.");
+                break;
+            case 2:
+                System.out.println("South America: Sao Paulo, Brazil");
+                break;
+            case 3:
+                System.out.println("Europe: Moscow, Russia");
+                break;
+            case 4:
+                System.out.println("Africa: Lagos, Nigeria");
+                break;
+            case 5:
+                System.out.println("Asia: Shanghai, China.");
+            case 6:
+                System.out.println("Australia: Sydney, Australia");
+                break;
+            case 7:
+                System.out.println("Antarctica: McMurdo Station, US.");
+                break;
+            default:
+                System.out.println("ned continent!");
+
+        }
+
+    }
+}
